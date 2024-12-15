2001ac51a1872f43c3964ed1b588e7da1677f1ca
Felipe Melo <felipemelozx@gmail.com>
2024-12-15 20:17:43 -0300

    feat: first commit

---
+++ src/main/java/com/felipemelozx/url_shortener_challenge/UrlShortenerChallengeApplication.java
@@ -0,0 +1,13 @@
+package com.felipemelozx.url_shortener_challenge;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class UrlShortenerChallengeApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(UrlShortenerChallengeApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/felipemelozx/url_shortener_challenge/UrlShortenerChallengeApplicationTests.java
@@ -0,0 +1,13 @@
+package com.felipemelozx.url_shortener_challenge;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class UrlShortenerChallengeApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
